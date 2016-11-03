ead1e85e9fd92568c74a730c26cf59d9e9e70dc3
Paulo César Mendoza Ochoa <mo.paulocesar@gmail.com>
2016-11-03 07:26:51 -0500

    Update point.cpp

--- Point/point.cpp
+++ Point/point.cpp
@@ -1,42 +1,72 @@
 #include "point.h"
 
+//Clase point: CONSTRUCTORES
+Point::Point(){
+    x = 0.0;
+    y = 0.0;
+}
+
+Point::Point(double a, double b){
+    x = a;
+    y = b;
+}
+
+Point::Point(Point &temp){
+    x = temp.x;
+    y = temp.y;
+}
+
+//Clase point: ATRIBUTOS
 double Point::getX() const{
-
-	return x;
-
+    return x;
 }
 
 double Point::getY() const{
-
-	return y;
-
+    return y;
 }
 
-void Point::setX(double nx){
-
-	x = nx;
-
+void Point::setX(double a){
+    x = a;
 }
 
-void Point::setY(double ny){
-
-	y = ny;
-
+void Point::setY(double b){
+    y = b;
 }
 
 void Point::printPoint(){
+    cout<<"("<<x<<" , "<<y<<")"<<endl;
+}
 
-	cout << "point(" << x  << ", "  << y << ")";
+void Point::modifyPoint(double a, double b){
+    x = a;
+    y = b;
+}
+
+//Clase PointArray: CONSTRUCTORES
+PointArray::PointArray(){
+    size = 0;
+    points=new Point[0];
+}
+
+PointArray::PointArray (const Point ptsToCopy[], const int toCopySize){
+
+    size = toCopySize ;
+    points = new Point [ toCopySize ];
+    for(int i = 0; i < toCopySize ; ++i)
+        points [i] = ptsToCopy [i];
 
 }
 
-void Point::modifyPoint(double nx, double ny){
 
-	x = nx;
-	y = ny;
+PointArray::PointArray(const PointArray &pv){
 
+    size = pv.size;
+    points = new Point[size];
+    for(int i = 0; i < size; i++)
+        points[i] = pv.points[i];
 }
 
+//Clase PointArray: ATRIBUTOS
 void PointArray::resize(int newSize){
 
 	Point *pts = new Point[newSize];
@@ -103,7 +133,7 @@ const Point *PointArray::get(const int pos) const{
 
 }
 
-void PointArray::printArr(){
+void PointArray::printArray(){
 
 	for(int i=0; i < size; i++)
 		cout << "point[" << i << "]: " << "( " << points[i].getX() << ", " << points[i].getY()<< " )" << endl;
